436a16c52ef2b1b3608c113b6a5a6bf3fb7ddfd0
maria45paula <paulaugggvbk@gmail.com>
2026-07-13 23:56:36 -0500

    Agregando ventana Principal

--- src/main/java/org/taller/Main.java
+++ src/main/java/org/taller/Main.java
@@ -1,9 +1,22 @@
 package org.taller;
 
+import views.CalculadoraEstudiante;
+import views.Principal;
+
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        JFrame home = new JFrame("Calculadora Estudiante");
+        CalculadoraEstudiante calculadora = new CalculadoraEstudiante();
+        Principal principal = new Principal();
+        home.setContentPane(principal.home);
+        home.pack();
+        home.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        home.setLocationRelativeTo(null);
+        home.setSize(600, 600);
+        home.setVisible(true);
     }
 }
---
+++ src/main/java/views/Principal.java
@@ -0,0 +1,11 @@
+package views;
+
+import javax.swing.*;
+
+public class Principal {
+    private JLabel Titulo;
+    private JButton calculadoraEstudianteButton;
+    private JButton calculadoraProfesorButton;
+    private JLabel descripcion;
+    public JPanel home;
+}
